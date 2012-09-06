9d1633ee2e132e2674442e4ca1f2e4ff9c1ba981
samidarko <samidarko@gmail.com>
2012-09-06 22:37:45 +0200

    elargissement des champs input et select des formulaires

--- semiocoder/semiocoder/encoder/views.py
+++ semiocoder/semiocoder/encoder/views.py
@@ -32,6 +32,7 @@ from django.http import Http404
 # TODO: revoir le nom des fichiers en sortie
 # TODO: control client : verifier la longueur des champs + options exclure le nom du fichier a la fin
 # TODO: dans le formulaire de selection de fichier ajouter une case "select all"
+# TODO: truncate au niveau des description des jobs
 
 @login_required(login_url=LOGIN_URL)
 def search(request):
